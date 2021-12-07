2fec65f23a89eda2d8dd2df4477a1b15a6113a4b
Pinny Geliebter <pgibralter@gmail.com>
2021-12-06 21:30:29 -0500

    adds PinnysProject view and renders some basic info

---
+++ src/views/PinnysProject.vue
@@ -0,0 +1,17 @@
+<template>
+  <ProjectInfo
+    title="Pinnys Project"
+    :contributor="{ name: 'Pinny Geliebter', email: 'pgibreeze@gmail.com' }"
+    description="does junk"
+  />
+  <h1>Hello World!!</h1>
+</template>
+<script>
+import ProjectInfo from "../components/ProjectInfo.vue"
+export default {
+  name: 'PinnysProject',
+  components: {
+    ProjectInfo
+  }
+}
+</script>
