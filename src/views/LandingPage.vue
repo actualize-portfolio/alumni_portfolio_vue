15445a4c9a6e5a8c0216da8e64e1df410b1a8622
James Gates <gatorjuice@gmail.com>
2021-12-25 17:14:12 -0600

    updated copy on home page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -5,31 +5,41 @@
         Alumni
         <span class="text-primary">Portfolio</span>
       </h1>
-      <div class="subheading mb-5">
+      <div class="subheading mb-3">
         Actualize Coding Bootcamp
         <a href="https://anyonecanlearntocode.com/">Learn More</a>
       </div>
       <h2 class="mb-2">Project Overview</h2>
       <p class="lead mb-3">
-        This web application is a project aimed at giving newer developers a
-        learning experience about some of the processes and conventions they may
-        encounter at their first companies. It's meant to be somewhat
-        opinionated about style, testing, and developer workflow.
+        This web application is the user-facing end of a project created by
+        myself, aimed at giving newer developers a learning experience about
+        some of the processes and conventions they may encounter at their first
+        companies while not being too overwhelming in its source code. It's
+        meant to be somewhat opinionated about style, testing, and developer
+        workflow, focusing on simple concepts and top-notch execution over any
+        single triumphant "idea for an app".
       </p>
       <p class="lead mb-3">
-        Engineers that contribute to this project are exposed to several key
-        concepts in preparation for onboarding wherever they may end up. They
-        will plan their work on epics/features/stories/bugs with a mock product
-        team, follow foundational Git practices, write quality code which must
-        pass peer review, provide tests for their contributions, and generally
-        treat the application as a first-class product.
+        Developers that contribute to this project are exposed to several key
+        concepts in preparation for professional onboarding. They will attribute
+        development time to stories, follow foundational Git practices, write
+        quality code and accompanying tests, elicit peer review, shepherd their
+        code all the way through CI-CD, and treat the application as a
+        first-class product. As an additional benefit of their participation,
+        developers will be able to contribute personal single-page application
+        snippets that showcase their talents while also demonstrating their
+        adherence to team development and stewardship of a high-quality code
+        base.
       </p>
-      <p class="lead mb-3">
-        As a benefit of their participation, developers will be able to
-        contribute portfolio pages featuring "code experience snippets" that
-        demonstrate their abilities and willingness to adhere to both a team
-        development strategy and the guidelines of a quality code base.
-      </p>
+      <p class="lead mb-0">Jamie Gates</p>
+      <small
+        >Creator, Senior Technical Lead,
+        <span class="text-primary">Actualize Alumn</span></small
+      >
+      <div
+        class="d-flex flex-column flex-md-row justify-content-between mb-5"
+      ></div>
+      <p class="lead mb-3"></p>
       <div class="flex-grow-1">
         <h3 class="mb-0">Front End</h3>
         <div class="subheading mb-3">
@@ -39,8 +49,17 @@
             >Vue.js<i class="fab fa-vuejs"></i
           ></a>
         </div>
-
-        <p>Fixme</p>
+        <p class="lead mb-3">
+          The client side runs on Vue.js 3. It utilizes Vuex for state
+          management and Vue Router for navigation. The usage of Vue is a
+          reflection of my recently discovered love of the framework itself.
+          Having mainly been a back end engineer since I began, I needed an
+          approachable solution for the front end. Vue has given me just that.
+        </p>
+        <p class="lead mb-3">
+          Please click on the navigation items to see contributions to the
+          project.
+        </p>
       </div>
       <div class="flex-shrink-0">
         <span class="text-primary"
@@ -57,7 +76,15 @@
         <div class="subheading mb-3">
           <a href="https://rubyonrails.org/">Ruby on Rails</a>
         </div>
-        <p>Fixme</p>
+        <p class="lead mb-3">
+          The server side is Rails version 6 running in API mode with a Postgres
+          database and a Sidekiq sidecar for asynchronous tasks. It uses a
+          service object interactor pattern and employs some basic
+          authentication with Bcrypt. It is hosted on Heroku.com at
+          <span
+            ><strong>https://alumni-portfolio-api.herokuapp.com/</strong></span
+          >
+        </p>
       </div>
       <div class="flex-shrink-0">
         <span class="text-primary"
