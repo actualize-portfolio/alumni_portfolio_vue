1e33a58749669983d9c992b40070f8a0280bebc4
James Gates <gatorjuice@gmail.com>
2021-12-31 21:37:19 -0600

    stub api responses

--- cypress/integration/favoriteBooks.spec.js
+++ cypress/integration/favoriteBooks.spec.js
@@ -2,6 +2,12 @@ describe("e2e", () => {
   it("can navigate to favoriteBooks and back", () => {
     cy.visit("/");
     cy.get(".nav-link").contains("Favorite Books").click();
+    cy.intercept("POST", "/api/v1/login", { fixture: "loginResponse" }).as(
+      "login"
+    );
+    cy.intercept("GET", "/api/v1/books", { fixture: "getBooks" }).as(
+      "getBooks"
+    );
     cy.get("#loginButton").click();
     cy.get("h1").contains("Favorite Books");
     cy.get(".img-profile").click();
