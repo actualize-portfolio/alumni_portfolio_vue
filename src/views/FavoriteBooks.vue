e7045dea36d72107bf13705876ee80a68287cc6a
James Gates <gatorjuice@gmail.com>
2022-01-02 18:48:50 -0600

    created pagination with known bug

--- src/views/FavoriteBooks.vue
+++ src/views/FavoriteBooks.vue
@@ -4,7 +4,41 @@
     :contributor="{ name: 'Jamie Gates', email: 'gatorjuice@gmail.com' }"
     description="Uses Vuex and a few backend calls to toggle some fake favorite books"
   />
-  <h4>Favorites Count: {{ favoritesCount() }}</h4>
+  <!-- Can't show favorites count until pagination is finished -->
+  <!-- <h4>Favorites Count: {{ favoritesCount() }}</h4> -->
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <router-link
+          v-if="currentPage > 1"
+          :to="`/books?page=${currentPage - 1}`"
+          class="page-link"
+          >Previous</router-link
+        >
+        <p v-else class="page-link disabled">Previous</p>
+      </li>
+      <li class="page-item">
+        <router-link :to="`/books?page=${currentPage - 1}`" class="page-link">{{
+          currentPage - 1
+        }}</router-link>
+      </li>
+      <li class="page-item">
+        <router-link :to="`/books?page=${currentPage}`" class="page-link">{{
+          currentPage
+        }}</router-link>
+      </li>
+      <li class="page-item">
+        <router-link :to="`/books?page=${currentPage + 1}`" class="page-link">{{
+          currentPage + 1
+        }}</router-link>
+      </li>
+      <li class="page-item">
+        <router-link :to="`/books?page=${currentPage + 1}`" class="page-link"
+          >Next</router-link
+        >
+      </li>
+    </ul>
+  </nav>
   <FavoriteBooksTable :books="books" />
 </template>
 
@@ -18,6 +52,16 @@ export default {
     ProjectInfo,
     FavoriteBooksTable,
   },
+  watch: {
+    "$route.query.page"() {
+      this.setCurrentPage(this.$route.query.page);
+    },
+  },
+  data() {
+    return {
+      currentPage: 0,
+    };
+  },
   computed: {
     books() {
       return this.$store.state.favoriteBooks.books;
@@ -27,10 +71,18 @@ export default {
     favoritesCount() {
       return this.books.filter((book) => book.is_favorite).length;
     },
+    setCurrentPage(page = 1) {
+      this.currentPage = Number.parseInt(page);
+      this.$store.dispatch("loadBooks", { page });
+    },
   },
   created() {
-    this.$store.dispatch("loadBooks");
+    this.setCurrentPage(this.$route.query.page);
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.disabled {
+  color: grey;
+}
+</style>
