486856d43f22c4a2d487191b7ee647616d1dac94
rvndyr <randyr2125@msn.com>
2022-11-21 20:06:09 -0500

    added logout with route to landing page

--- src/components/TheSessionLink.vue
+++ src/components/TheSessionLink.vue
@@ -2,7 +2,15 @@
   <div id="login" class="navlink">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link to="/login" class="nav-link">Log In</router-link>
+        <a
+          @click.prevent="destroySession()"
+          v-if="userLoggedIn()"
+          to="/logout"
+          class="nav-link"
+        >
+          Log Out
+        </a>
+        <router-link v-else to="/login" class="nav-link">Log In</router-link>
       </li>
     </ul>
   </div>
@@ -10,6 +18,15 @@
 <script>
 export default {
   name: "TheSessionLink",
+  methods: {
+    userLoggedIn() {
+      //Access the store for the JWT and return true if jtw exists
+      return !this.$store.state.jwt;
+    },
+    destroySession() {
+      this.$store.dispatch("logout");
+    },
+  },
 };
 </script>
 <style scoped></style>
