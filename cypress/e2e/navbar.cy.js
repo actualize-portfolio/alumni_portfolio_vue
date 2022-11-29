6d040483fec05013c0c922b2aa5bc0ffffa93ba6
rvndyr <randyr2125@msn.com>
2022-11-29 14:25:31 -0500

    moved timeout to element on page load

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -1,7 +1,8 @@
 describe("click login link", () => {
   it("directs us to the login page and changes the link to logout and redirects to landing page.", () => {
     cy.clearLocalStorage();
-    cy.visit("/", { timeout: 30000 });
+    cy.visit("/");
+    cy.get("#login", { timeout: 30000 });
     cy.get("#login").contains("Log In").click();
     cy.intercept("POST", "/api/v1/login", {
       statusCode: 200,
