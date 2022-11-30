ce9a598836d00b8c7cbe2c713209b2cb5563f656
James Gates <gatorjuice@gmail.com>
2022-11-30 17:54:03 -0600

    attempting to click on element returned in promise

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -2,8 +2,10 @@ describe("click login link", () => {
   it("directs us to the login page and changes the link to logout and redirects to landing page.", () => {
     cy.clearLocalStorage();
     cy.visit("/");
-    cy.get("#login", { timeout: 30000 });
-    cy.get("#login").contains("Log In").click();
+    cy.get("#login");
+    cy.get("#login").contains("Log In");
+    cy.get("#login").click();
+
     cy.intercept("POST", "/api/v1/login", {
       statusCode: 200,
       fixture: "loginResponse",
@@ -14,7 +16,9 @@ describe("click login link", () => {
     cy.get("#loginButton").click();
 
     cy.wait("@login");
-    cy.get("#login").contains("Log Out").click();
+    cy.get("#login")
+      .contains("Log Out")
+      .then(($el) => $el.click());
     cy.location("pathname").should("eq", "/");
   });
 });
