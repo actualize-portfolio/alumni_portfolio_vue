b39eaff3f290d591a50f31768119b47bfa990b36
James <gatorjuice@gmail.com>
2023-02-19 22:19:07 -0600

    remove button, change background and border

--- src/views/SunnySorter.vue
+++ src/views/SunnySorter.vue
@@ -1,6 +1,7 @@
 <template>
   <ProjectInfo title="Sunny Sorter" :contributor="{ name: 'Jamie Gates', email: 'gatorjuice@gmail.com' }"
     description="Tabulates the best episodes of Sunny" />
+  <h4 class="question">Which episode do you like more?</h4>
   <div class="row">
     <div v-for="episode in $store.state.sunnySorter.episodes" :key="episode.id" class="col">
       <SunnyEpisode :episode="episode" />
@@ -65,5 +66,7 @@ export default {
 };
 </script>
 <style scoped>
-
+  .question {
+    text-align: center;
+  }
 </style>
